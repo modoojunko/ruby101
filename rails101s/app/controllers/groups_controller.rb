6d37b2da16f9a852abf5cff46e36f6cd30652119
alexzhuke <alexee_zhu@163.com>
2015-08-25 23:16:38 +0800

    add helper

--- rails101s/app/controllers/groups_controller.rb
+++ rails101s/app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
     def index
+      flash[:notice] = "早安！你好！"
+      flash[:alert] = "早安！你好！"
+      flash[:warning] = "這是 warning 訊息！"
     end
 end
