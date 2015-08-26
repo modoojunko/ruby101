070edaf2bd38966bfe2271847897eddeeb38dc27
alexzhuke <alexee_zhu@163.com>
2015-08-26 22:16:03 +0800

    manually create CRUD

--- rails101s/app/controllers/groups_controller.rb
+++ rails101s/app/controllers/groups_controller.rb
@@ -1,7 +1,51 @@
 class GroupsController < ApplicationController
+
     def index
-      flash[:notice] = "早安！你好！"
-      flash[:alert] = "早安！你好！"
-      flash[:warning] = "這是 warning 訊息！"
+        @groups = Group.all
     end
+
+    def show
+        @group = Group.find(params[:id])
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def create
+        @group = Group.create(group_params)
+
+        if @group.save
+            redirect_to groups_path,notice: '新增討論版成功'
+        else
+            render :new
+        end
+    end
+
+    def update
+        @group = Group.find(params[:id])
+
+        if @group.update(group_params)
+            redirect_to groups_path, notice: "修改成功"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        redirect_to groups_path, alert: "删除成功"
+    end
+
+end
+
+private
+
+def group_params
+    params.require(:group).permit(:title, :description)
 end
